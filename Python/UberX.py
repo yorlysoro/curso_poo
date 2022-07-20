071beb7a8aae5cf655a929631a8c3812da36ef43
yorlysoro <yorlysoro@gmail.com>
2022-07-20 10:47:31 -0400

    End of course

---
+++ Python/UberX.py
@@ -0,0 +1,11 @@
+from car import Car
+
+class UberX(car):
+	brand = str
+	model = str
+
+	def __init__(self, license, driver, brand, driver):
+		super().__init__(license,driver)
+		self.brand = brand
+		self.model = model
+
